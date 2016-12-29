e8bdd0bfef8ed71b541058cfeef6ef31854fe869
leishmilypeng <891880796@qq.com>
2016-12-29 13:51:06 +0800

    mina netty

--- src/main/java/com/lp/test/proxy/HelloWorldImpl.java
+++ src/main/java/com/lp/test/proxy/HelloWorldImpl.java
@@ -2,7 +2,6 @@ package com.lp.test.proxy;
 
 public class HelloWorldImpl implements HelloWorld {
 
-	@Override
 	public void sayHelloWorld() {
 		// TODO Auto-generated method stub
 		System.out.println("HelloWorld!");
